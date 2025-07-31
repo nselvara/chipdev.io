a228fcba22e44dd7086cde983a590563ceff8630
nselvara <43971369+nselvara@users.noreply.github.com>
2025-07-31 09:57:51 +0000

    Add CI/CD Fixes #5

--- ip/test_runner.py
+++ ip/test_runner.py
@@ -5,20 +5,25 @@ It functions as a wrapper to not bother the user with the details of VUnit.
 Author: N. Selvarajah
 """
 
+import sys
+import os
+
 from utils.run_all_testbenches_lib import main as run_all_testbenches_lib
 from utils.run_all_testbenches_lib import bcolours
 
 def run_all_testbenches():
     returncode = run_all_testbenches_lib(
-        path="./ip/",
-        tb_pattern="**",
-        timeout_ms=1.0,
-        gui=False,
-        compile_only=False,
-        clean=False,
-        debug=False,
-        use_xilinx_libs=False,
-        use_intel_altera_libs=False
+        path="./ip/",                 # Path where the HDL & tb files are located
+        tb_pattern="**",              # Match all testbenches
+        timeout_ms=1.0,               # Timeout in milliseconds
+        gui=False,                    # Set to True to open ModelSim/QuestaSim GUI
+        compile_only=False,           # Only compile, don't run simulations
+        clean=False,                  # Clean before building
+        debug=False,                  # Enable debug logging
+        use_xilinx_libs=True,         # Add Xilinx simulation libraries, note set it true to load glbl module
+        use_intel_altera_libs=False,  # Add Intel/Altera simulation libraries
+        excluded_list=[],             # List of testbenches to exclude
+        xunit_xml=None                # Output file for test results
     )
     print(
         f"hdl_offline_tests: {bcolours.OKGREEN + 'Passed' if returncode == 0 else bcolours.FAIL + 'Failed'}{bcolours.ENDC}"
